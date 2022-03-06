85ad5dcc5269f439912d2da08f2879a9be4ade05
magurman <mattgurman@gmail.com>
2022-03-06 17:52:08 -0500

    update landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -21,24 +21,23 @@ class Landing extends Component {
                         </p>
                         <p className="landing-p">
                             I'm currently a backend software engineer at <a href={data.links.seedfi} target="_blank" rel="noopener noreferrer">SeedFi</a>.
-                            Within tech, my greatest interests are distributed systems and how they scale. 
                         </p>
                         <p className="landing-p">
-                            One of my favorite things to do is shoot {PhotographyLink}.
+                            One of my biggest hobbies is shooting {PhotographyLink}.
                         </p>
                         <p className="landing-p">
-                            I love food, both cooking and eating it. My favorite {CuisineLink} is Mexican because they have the best dish in the world: tacos.
+                            I love <a href="/cuisine">food</a>, both cooking and eating it. My favorite dish is tacos. 
                         </p>
                         <p className="landing-p">
                             Check out some of my favorite <a href="/music">music</a> and what I like so much about it.
                         </p>
                         <p className="landing-p">
-                            I love <a href="/podcasts">podcasts</a> and discussing ones I find interesting with friends. I think podcasts are an underratedly impactful 
+                            I listen to a lot of <a href="/podcasts">podcasts</a> and enjoy discussing ones I find interesting with friends. I think podcasts are an underratedly impactful 
                             internet-enabled technology.
                         </p>
-                        <p className="landing-p">
+                        {/* <p className="landing-p">
                             I don't read as much as I should, but I have some favorite <a href="/books">books</a> and some thoughts to go with them.
-                        </p>                        
+                        </p>                         */}
                         <p className="landing-p">
                             Lastly, I wanted a place to keep track of my random <a href="/thoughts">thoughts</a> and muse about some open-ended <a href="/questions">questions</a> I find interesting.
                         </p>
